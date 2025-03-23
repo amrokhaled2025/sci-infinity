0b181e66d8979fa5f3e058211691b6d32f28b018
amr khaled <amrok1666@gmail.com>
2025-03-23 20:54:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# sci-infinity" 
+"# sci-infinity" 
+"# sci-infinity" 
---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Change the header text on page load
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header h1');
+  header.textContent = 'مرحبًا بكم في مبادرة Scinfinity';
+});
